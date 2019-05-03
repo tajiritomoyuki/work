c653633f0ba4b69d76e5898374446d37512634ba
tajiri <tajiri_leader@nagasebros.com>
2019-05-03 13:10:30 +0900

    a

--- make_lcs.py
+++ make_lcs.py
@@ -2,15 +2,23 @@
 import os
 import glob
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 from multiprocessing as mp
 import h5py
 
-dirtype = "step3"
-h5dir = "/pike/pipeline/%s" % dirtype
-dstdir = "/home/tajiri/tess/nn/data"
+rootdir = os.path.abspath(os.path.dirname(__file__))
+csvpath = os.path.join(rootdir, "csv", "labeled.csv")
+pldir = "/pike/pipeline"
+dstdir = os.path.join(rootdir, "data")
 
-def load(h5path):
+def load(path):
+    h5path1 = os.path.join(pldir, "step3", path)
+    h5path2 = os.path.join(pldir, "TIC3", path)
+    if os.path.exists(h5path1):
+        h5path = h5path1
+    else:
+        h5path = h5path2
     with h5py.File(h5path, "r") as f:
         flux = np.array(f["LC"]["SAP_FLUX"])
         quality = np.array(f["LC"]["QUALITY"])
@@ -25,22 +33,22 @@ def load(h5path):
     return lc_interp, os.path.basename(h5path)
 
 def main():
-    for sector in range(1, 8)
-        h5list = glob.glob(os.path.join(h5dir, "*_%s_?_?.h5" % sector))
+    df = pd.read_csv(csvpath, names=["path", "label"])
+    f_sector = lambda x: int(x.split("_")[2])
+    sector_df = df["path"].map(f_sector)
+    df["sector"] = sector
+    for label, sector in product([0, 1], [1, 2, 3, 5]):
+        tar_df = df[(df["sector"] == sector) & (df["label"] == label)]
+        path_list = tar_df["path"].values
         lc_list = []
-        path_list = []
         #読み込み
         with mp.Pool(mp.cpu_count()) as p:
-            for lc, path in p.map(load, tqdm(h5list)):
+            for lc, path in p.map(load, tqdm(path_list)):
                 lc_list.append(lc)
-                path_list.append(path)
         lc_array = np.vstack(tuple(lc_list))
-        path_array = np.hstack(tuple(path_list))
         #書き出し
-        dstpath = os.path.join(dstdir, "data_%s.h5" % sector)
-        with h5py.File(dstpath, "w") as f:
-            f.create_dataset("lc", data=lc_array)
-            f.create_dataset("path", data=path_array)
+        dstpath = os.path.join(dstdir, "%s_%s.npz" % (sector, label))
+        np.savez(dstpath, data=lc_array)
 
 if __name__ == '__main__':
     main()
